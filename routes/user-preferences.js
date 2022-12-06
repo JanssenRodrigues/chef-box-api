5368cd033ffecf14bb57db5dd4b5b36a72e7523b
Janssen Rodrigues <janssen.dev@gmail.com>
2022-12-06 09:38:22 -0300

    Finaliza rotas de preferencias do usuário

--- routes/user-preferences.js
+++ routes/user-preferences.js
@@ -35,7 +35,7 @@ router.get("/", (req, res, next) => {
 router.post("/", (req, res, next) => {
   mysql.getConnection((error, conn) => {
     const { userId, preferences } = req.query;
-    console.log(req.query);
+
     conn.query(
       "INSERT INTO preferences (user_id, content) values (?,?)",
       [userId, preferences],
@@ -55,7 +55,37 @@ router.post("/", (req, res, next) => {
         }
 
         res.status(200).send({
-          preferences: result[0],
+          preferences: JSON.parse(result[0]),
+        });
+      }
+    );
+  });
+});
+
+router.patch("/", (req, res, next) => {
+  mysql.getConnection((error, conn) => {
+    const { userId, preferences } = req.query;
+
+    conn.query(
+      "UPDATE preferences SET content = ? WHERE user_id = ?",
+      [preferences, userId],
+      (error, result, field) => {
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error,
+          });
+        }
+
+        if (result.length === 0) {
+          return res.status(401).send({
+            message: "Usuário não possui preferências cadastradas",
+          });
+        }
+
+        res.status(200).send({
+          message: "Preferências atualizadas com sucesso.",
         });
       }
     );
