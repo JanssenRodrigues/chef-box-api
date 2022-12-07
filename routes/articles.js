d8541c7f8bf686a5399bae6a611ae9cd44699640
Janssen Rodrigues <janssen.dev@gmail.com>
2022-12-07 11:46:28 -0300

    Inicia rotas de avaliação de receita

--- routes/articles.js
+++ routes/articles.js
@@ -27,7 +27,6 @@ router.get("/", (req, res, next) => {
 });
 
 router.get("/:url", (req, res, next) => {
-  console.log(req.params.url);
   mysql.getConnection((error, conn) => {
     conn.query(
       "SELECT * FROM articles WHERE url = ?",
@@ -44,6 +43,66 @@ router.get("/:url", (req, res, next) => {
         if (result.length === 0) {
           return res.status(401).send({
             message: "Não existem receitas cadastradas",
+          });
+        }
+
+        res.status(200).send({
+          revenue: result[0],
+        });
+      }
+    );
+  });
+});
+
+router.post("/review", (req, res, next) => {
+  const { articleId, username, comment, rate } = req.query;
+  console.log(req.query);
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "INSERT INTO article_reviews (article_id, username, comment, rate) VALUES (?,?,?,?)",
+      [articleId, username, comment, rate],
+      (error, result, field) => {
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error,
+          });
+        }
+
+        if (result.length === 0) {
+          return res.status(401).send({
+            message: "Falha em cadastrar avaliação",
+          });
+        }
+
+        res.status(200).send({
+          revenue: result[0],
+        });
+      }
+    );
+  });
+});
+
+router.get("/review/user", (req, res, next) => {
+  const { articleId, username } = req.query;
+  console.log(req.query);
+  mysql.getConnection((error, conn) => {
+    conn.query(
+      "SELECT * FROM article_reviews WHERE username = ? AND article_id = ?",
+      [username, articleId],
+      (error, result, field) => {
+        conn.release();
+
+        if (error) {
+          return res.status(500).send({
+            error,
+          });
+        }
+
+        if (result.length === 0) {
+          return res.status(401).send({
+            message: "Avaliação não cadastrada",
           });
         }
 
